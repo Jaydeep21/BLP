092cd2ddc11c6a23e022daa9b6201e03f354799e
Jaydeep Dharamsey <33755235+Jaydeep21@users.noreply.github.com>
2019-01-25 10:27:24 +0530

    adding database

---
+++ blp.sql
@@ -0,0 +1,536 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 24, 2019 at 12:05 PM
+-- Server version: 5.7.21-log
+-- PHP Version: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addiction`
+--
+
+CREATE TABLE `addiction` (
+  `pid` bigint(20) NOT NULL,
+  `addiction_type` varchar(50) NOT NULL,
+  `addiction_start` varchar(20) NOT NULL,
+  `addiction_end` varchar(20) NOT NULL,
+  `addiction_frequency` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `pid` bigint(20) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `address_type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clinic`
+--
+
+CREATE TABLE `clinic` (
+  `cid` bigint(20) NOT NULL,
+  `cname` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `area_code` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `pid` bigint(11) NOT NULL,
+  `mobile` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drugs`
+--
+
+CREATE TABLE `drugs` (
+  `did` int(11) NOT NULL,
+  `dname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drugs_prescribed`
+--
+
+CREATE TABLE `drugs_prescribed` (
+  `tid` bigint(20) NOT NULL,
+  `did` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `dosage` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `family_history`
+--
+
+CREATE TABLE `family_history` (
+  `pid` bigint(11) NOT NULL,
+  `relation` varchar(20) NOT NULL,
+  `disease` varchar(20) NOT NULL,
+  `mmyy_diagnosis` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `known_leprosy_contacts`
+--
+
+CREATE TABLE `known_leprosy_contacts` (
+  `pid` bigint(20) NOT NULL,
+  `type_leprosy` varchar(20) NOT NULL,
+  `treatment` varchar(100) NOT NULL,
+  `duration_treatment` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leprosy_relations`
+--
+
+CREATE TABLE `leprosy_relations` (
+  `pid` bigint(20) NOT NULL,
+  `klpid` bigint(20) NOT NULL,
+  `relation` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medical_history`
+--
+
+CREATE TABLE `medical_history` (
+  `pid` bigint(20) NOT NULL,
+  `disease` varchar(100) NOT NULL,
+  `mmyy_diagnosis` varchar(20) NOT NULL,
+  `mmyy_curation` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obstetric_history`
+--
+
+CREATE TABLE `obstetric_history` (
+  `pid` bigint(20) NOT NULL,
+  `number_child` int(11) NOT NULL,
+  `gender_child` varchar(10) NOT NULL,
+  `mmmyy_birth` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `past_drugs`
+--
+
+CREATE TABLE `past_drugs` (
+  `pid` bigint(20) NOT NULL,
+  `hospital` varchar(50) NOT NULL,
+  `drugs` varchar(20) NOT NULL,
+  `mmyy_start` varchar(20) NOT NULL,
+  `mmyy_end` varchar(20) NOT NULL,
+  `dosage` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `pid` bigint(20) NOT NULL,
+  `referred` tinyint(1) NOT NULL,
+  `patient_code` varchar(20) NOT NULL,
+  `diagnosed` tinyint(1) NOT NULL,
+  `allergy` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peripheral_nerves`
+--
+
+CREATE TABLE `peripheral_nerves` (
+  `tid` bigint(20) NOT NULL,
+  `nerves` varchar(20) NOT NULL,
+  `thickness` varchar(20) NOT NULL,
+  `tenderness` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `person`
+--
+
+CREATE TABLE `person` (
+  `pid` bigint(20) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `mname` varchar(30) NOT NULL,
+  `lname` varchar(30) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` char(2) NOT NULL,
+  `occupation` varchar(30) NOT NULL,
+  `no_of_family_members` int(11) NOT NULL,
+  `person_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `person_updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pregnant_since`
+--
+
+CREATE TABLE `pregnant_since` (
+  `pid` bigint(20) NOT NULL,
+  `mmyy_pregnant_since` varchar(20) NOT NULL,
+  `sys_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reactions`
+--
+
+CREATE TABLE `reactions` (
+  `tid` bigint(20) NOT NULL,
+  `type_reaction` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sensory_test`
+--
+
+CREATE TABLE `sensory_test` (
+  `tid` bigint(20) NOT NULL,
+  `pinprick` varchar(20) NOT NULL,
+  `touch` varchar(20) NOT NULL,
+  `temperature` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `smear_test`
+--
+
+CREATE TABLE `smear_test` (
+  `tid` bigint(20) NOT NULL,
+  `site` varchar(50) NOT NULL,
+  `mi` float NOT NULL,
+  `bi` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `treatment_record`
+--
+
+CREATE TABLE `treatment_record` (
+  `pid` bigint(20) NOT NULL,
+  `tid` bigint(20) NOT NULL,
+  `date_attend` date NOT NULL,
+  `opthalmic_involvement` varchar(50) NOT NULL,
+  `rep` varchar(50) NOT NULL,
+  `clf` varchar(50) NOT NULL,
+  `eyes` varchar(50) NOT NULL,
+  `disability` varchar(50) NOT NULL,
+  `motor_testing` varchar(50) NOT NULL,
+  `skin_lesions` varchar(50) NOT NULL,
+  `complaints` varchar(50) NOT NULL,
+  `result` varchar(50) NOT NULL,
+  `type_diagnosed` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addiction`
+--
+ALTER TABLE `addiction`
+  ADD PRIMARY KEY (`addiction_type`,`pid`),
+  ADD KEY `pid` (`pid`);
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`pid`,`address`);
+
+--
+-- Indexes for table `clinic`
+--
+ALTER TABLE `clinic`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`pid`,`mobile`);
+
+--
+-- Indexes for table `drugs`
+--
+ALTER TABLE `drugs`
+  ADD PRIMARY KEY (`did`);
+
+--
+-- Indexes for table `drugs_prescribed`
+--
+ALTER TABLE `drugs_prescribed`
+  ADD PRIMARY KEY (`tid`),
+  ADD KEY `did` (`did`);
+
+--
+-- Indexes for table `family_history`
+--
+ALTER TABLE `family_history`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `known_leprosy_contacts`
+--
+ALTER TABLE `known_leprosy_contacts`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `leprosy_relations`
+--
+ALTER TABLE `leprosy_relations`
+  ADD PRIMARY KEY (`pid`,`klpid`),
+  ADD KEY `klpid` (`klpid`);
+
+--
+-- Indexes for table `medical_history`
+--
+ALTER TABLE `medical_history`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `obstetric_history`
+--
+ALTER TABLE `obstetric_history`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `past_drugs`
+--
+ALTER TABLE `past_drugs`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`pid`),
+  ADD UNIQUE KEY `patient_code` (`patient_code`);
+
+--
+-- Indexes for table `peripheral_nerves`
+--
+ALTER TABLE `peripheral_nerves`
+  ADD PRIMARY KEY (`tid`,`nerves`);
+
+--
+-- Indexes for table `person`
+--
+ALTER TABLE `person`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `pregnant_since`
+--
+ALTER TABLE `pregnant_since`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `reactions`
+--
+ALTER TABLE `reactions`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- Indexes for table `sensory_test`
+--
+ALTER TABLE `sensory_test`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- Indexes for table `smear_test`
+--
+ALTER TABLE `smear_test`
+  ADD PRIMARY KEY (`tid`,`site`);
+
+--
+-- Indexes for table `treatment_record`
+--
+ALTER TABLE `treatment_record`
+  ADD PRIMARY KEY (`tid`),
+  ADD KEY `treatment_record_ibfk_1` (`pid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `person`
+--
+ALTER TABLE `person`
+  MODIFY `pid` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `addiction`
+--
+ALTER TABLE `addiction`
+  ADD CONSTRAINT `addiction_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `contact`
+--
+ALTER TABLE `contact`
+  ADD CONSTRAINT `contact_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `drugs_prescribed`
+--
+ALTER TABLE `drugs_prescribed`
+  ADD CONSTRAINT `drugs_prescribed_ibfk_1` FOREIGN KEY (`tid`) REFERENCES `treatment_record` (`tid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `drugs_prescribed_ibfk_2` FOREIGN KEY (`did`) REFERENCES `drugs` (`did`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `family_history`
+--
+ALTER TABLE `family_history`
+  ADD CONSTRAINT `family_history_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `known_leprosy_contacts`
+--
+ALTER TABLE `known_leprosy_contacts`
+  ADD CONSTRAINT `known_leprosy_contacts_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `leprosy_relations`
+--
+ALTER TABLE `leprosy_relations`
+  ADD CONSTRAINT `leprosy_relations_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `leprosy_relations_ibfk_2` FOREIGN KEY (`klpid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `medical_history`
+--
+ALTER TABLE `medical_history`
+  ADD CONSTRAINT `medical_history_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `obstetric_history`
+--
+ALTER TABLE `obstetric_history`
+  ADD CONSTRAINT `obstetric_history_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `patient`
+--
+ALTER TABLE `patient`
+  ADD CONSTRAINT `patient_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `peripheral_nerves`
+--
+ALTER TABLE `peripheral_nerves`
+  ADD CONSTRAINT `peripheral_nerves_ibfk_1` FOREIGN KEY (`tid`) REFERENCES `treatment_record` (`tid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pregnant_since`
+--
+ALTER TABLE `pregnant_since`
+  ADD CONSTRAINT `pregnant_since_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sensory_test`
+--
+ALTER TABLE `sensory_test`
+  ADD CONSTRAINT `sensory_test_ibfk_1` FOREIGN KEY (`tid`) REFERENCES `treatment_record` (`tid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `smear_test`
+--
+ALTER TABLE `smear_test`
+  ADD CONSTRAINT `smear_test_ibfk_1` FOREIGN KEY (`tid`) REFERENCES `treatment_record` (`tid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `treatment_record`
+--
+ALTER TABLE `treatment_record`
+  ADD CONSTRAINT `treatment_record_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `person` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
