91941b6f5247e306a1b550993d8db3228808e8bc
Poojan <32042207+poojn@users.noreply.github.com>
2019-01-29 16:49:52 +0530

    php testing

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$dbname='blp';
+$dbhost='localhost';
+$dbpass='';
+$dbuser='root';
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if(!$conn){
+    echo "Connection error".die(mysqli_connection_error());
+}
+else {
+	echo "connection successful<br/>";
+}
+
+?>
