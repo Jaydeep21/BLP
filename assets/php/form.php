203ea2488322b2299581a90f2171a68bafbba4a7
Jaydeep Dharamsey <33755235+Jaydeep21@users.noreply.github.com>
2019-03-02 15:40:00 +0530

    Add files via upload
    updated form with one change left

--- assets/php/form.php
+++ assets/php/form.php
@@ -2,17 +2,58 @@
 
 include('connection.php');
 
-$
 
-$qry="insert into  values";
+$name = $_POST['Name'];
+$dob = $_POST['DOB'];
+$aadhar = $_POST['aadhar'];
+$sex = $_POST['Sex'];
+date_default_timezone_set('Asia/Kolkata');
+$date = date('Y-m-d H:i:s');
 
-$result = mysqli_query($conn,$qry);
+
+// echo $date;
+
+$qry1 = "INSERT INTO `person` (`fname`, `dob`, `gender`, `aadhar`, `person_updated`) VALUES ('$name', '$dob', '$sex', $aadhar, '$date')";
+$qry2
+$qry3
+$qry4
+$qry5
+$qry6
+$qry7
+$qry8
+$qry9
+$qry10
+$qry11
+$qry12
+$qry13
+$qry14
+$qry15
+$qry16
+$qry17
+$qry18
+$qry19
+$qry20
+
+
+
+
+
+
+$result = mysqli_query($conn,"show tables");
+$insert = mysqli_query($conn, $qry);
 
 if($result){
-    header("location:http//:localhost/BLP/index.html");
+    // header("location:http//:localhost/BLP/index.html");
+    while ($row = mysqli_fetch_assoc($result)) {
+    	echo "<br/>".$row['Tables_in_blp'];
+    }
+}
+if($insert){
+	echo"Successful Insertion";
 }
 else{
     echo"Query error";
 }
 
+echo "<br/>*".$name."*".$dob." ".$aadhar." ".$sex." ".$date;
 ?>
---
+++ assets/php/login.php
@@ -0,0 +1,33 @@
+<?php
+
+	session_start();
+	include('connection.php');
+
+	$name = mysqli_real_escape_string($conn,$_POST['name']);
+	
+	$password = $_POST['password'];
+	
+	$qry = " select * from user where name='$name' and password='$password' ";
+	$result=mysqli_query($conn,$qry);
+
+	if(!$result){
+		echo $qry;	
+	}
+	else{
+		if($row= mysqli_fetch_assoc($result)){
+			$_SESSION['name'] = $name;
+			$_SESSION['password'] = $password;
+			echo "<script>alert('Welcome $name');
+				window.location.href = '../../';
+				</script>";
+			
+		}
+		else{
+
+			echo "<script>alert('Wrong Credentials');
+					window.location.href = '../../modules/login.php';
+					</script>";
+		}	
+	
+	}
+?>
